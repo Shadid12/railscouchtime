82dff1227cbabe3af330c7f4fc478832e192b9be
Shadid12 <shadid.haque@uoit.net>
2017-01-09 08:39:13 +0000

    mongo connected

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,7 @@
-class Listing < ApplicationRecord
+class Listing
+    include Mongoid::Document
+    store_in collection: "listings"
+    field :title, type: String
+    field :url, type: String
+    field :cover, type: String
 end
